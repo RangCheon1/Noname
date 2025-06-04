afed8e87522522a739d9babc533924636248c30e
RangCheon1 <rydbrdyd95@gmail.com>
2025-06-04 16:05:57 +0900

    Delete elect directory

--- elect/src/main/webapp/resources/js/select.js
+++
@@ -1,8 +0,0 @@
-function validateSearch() {
-    var keyword = document.getElementsByName("keyword")[0].value.trim();
-    if (keyword === "") {
-        alert("검색어를 입력해주세요.");
-        return false;
-    }
-    return true;
-}
